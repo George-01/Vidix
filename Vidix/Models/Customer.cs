832ed443031a9b8ce041f49055fd2f18a93b53d7
George-01 <george.coker@hotmail.com>
2019-05-27 19:26:18 +0100

    Membership Business Rules and Validations completed.

--- Vidix/Models/Customer.cs
+++ Vidix/Models/Customer.cs
@@ -10,11 +10,12 @@ namespace Vidix.Models
     {
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Please enter customer's name.")]
         [StringLength(255)]
         public string Name { get; set; }
 
         [Display(Name = "Date of Birth")]
+        [Min18YearsIfAMember]
         public DateTime? Birthdate { get; set; }
 
         public bool IsSubscribedToNewsLetter { get; set; }
